e993cdb37779a248be3f302fa44226f9f76adfe5
ChrisKheng <chriskheng1609@gmail.com>
2021-09-28 20:13:12 +0800

    Set up test for RelatedCustomerTransaction

--- src/test/java/cs4224/transactions/RelatedCustomerTransactionTest.java
+++ src/test/java/cs4224/transactions/RelatedCustomerTransactionTest.java
@@ -1,12 +1,19 @@
 package cs4224.transactions;
 
 import cs4224.extensions.InitializationExtension;
+import cs4224.utils.Utils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith({InitializationExtension.class})
 public class RelatedCustomerTransactionTest {
+    @BeforeAll
+    public static void setup() {
+        Utils.executeBashCommand("cqlsh -f src/test/resources/data/related_customer_transaction/load_data.cql");
+    }
+
     @Test
     public void testExecuteHasRelatedCustomer() {
         Assertions.assertEquals(1, 1);
