050b321f4b673dd055fb5d03dd3c9eaa6117121f
Yuu <24975893+YuuYouYoo@users.noreply.github.com>
2021-10-04 04:15:02 +0800

    fixed the bugs in the implementation of StockLevelTransaction

--- src/main/java/cs4224/transactions/StockLevelTransaction.java
+++ src/main/java/cs4224/transactions/StockLevelTransaction.java
@@ -3,6 +3,7 @@ package cs4224.transactions;
 import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.CqlSession;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class StockLevelTransaction extends BaseTransaction {
@@ -19,29 +20,35 @@ public class StockLevelTransaction extends BaseTransaction {
         final int noOfOrders = Integer.parseInt(parameters[4]);
 
         final int districtNextOrderId = session.execute(
-                "SELECT D_NEXT_O_ID FROM stock WHERE W_ID = ? AND D_ID = ?",
+                "SELECT D_NEXT_O_ID FROM district WHERE D_W_ID = ? AND D_ID = ?",
                 warehouseId,
                 districtId
         ).one().getInt(CqlIdentifier.fromCql("D_NEXT_O_ID"));
 
-        long count = session.execute(
-                        "SELECT S_QUANTITY FROM stock WHERE S_W_ID = ? AND S_I_ID IN (?)",
+        List<Integer> matchingOrderLineItemIds =
+                session.execute(
+                        "SELECT OL_I_ID FROM order_line " +
+                                "WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID >= ? AND OL_O_ID < ?",
                         warehouseId,
-                        session.execute(
-                                        "SELECT OL_I_ID FROM order_line " +
-                                                "WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID >= ? AND OL_O_ID < ?",
-                                        warehouseId,
-                                        districtId,
-                                        districtNextOrderId - noOfOrders,
-                                        districtNextOrderId)
-                                .all()
-                                .stream()
-                                .map(row -> row.getInt(CqlIdentifier.fromCql("OL_I_ID")))
-                                .distinct()
-                                .collect(Collectors.toList()))
+                        districtId,
+                        districtNextOrderId - noOfOrders,
+                        districtNextOrderId)
                 .all()
                 .stream()
-                .filter(row -> row.getInt(CqlIdentifier.fromCql("S_QUANTITY")) < threshold)
+                .map(row -> row.getInt(CqlIdentifier.fromCql("OL_I_ID")))
+                .distinct()
+                .collect(Collectors.toList());
+
+        long count =
+                session.execute(
+                        session.prepare(
+                        "SELECT S_QUANTITY FROM stock WHERE S_W_ID = ? AND S_I_ID IN ?")
+                                .bind()
+                                .setInt(0, warehouseId)
+                                .setList(1, matchingOrderLineItemIds, Integer.class))
+                .all()
+                .stream()
+                .filter(row -> row.getBigDecimal(CqlIdentifier.fromCql("S_QUANTITY")).intValue() < threshold)
                 .count();
 
         System.out.printf("Number of items with stock quantities below the threshold: %d%n", count);
